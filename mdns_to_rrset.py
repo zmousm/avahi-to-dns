5e5190be88e6ca0d1f59bcf272c4890d9eee9995
Zenon Mousmoulas <zmousm@noc.grnet.gr>
2013-05-24 18:10:05 +0300

    minor improvements

--- mdns_to_rrset.py
+++ mdns_to_rrset.py
@@ -1,28 +1,42 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+#import cgi
+#import cgitb; cgitb.enable()
 import zeroconf
 import dns.name
 import dns.resolver
 import dns.reversename
 import re
+from dns.rdatatype import *
+from dns.exception import DNSException
 
-def dnssd_search(stype='_ipp._tcp'):
+
+#print
+
+def dnssd_search(stype='_http._tcp'):
     dnssd = zeroconf.search(name=None, type=stype, domain="local")
 
     for key in dnssd.keys():
-      inst_type = key[1]
-      inst_fullname = dns.name.from_text(key[0] + '.' + key[1]).to_text(omit_final_dot=True)
-      inst_port = dnssd[key]['port']
-      inst_hostname = dns.resolver.query(dns.reversename.from_address(dnssd[key]['address']), 'PTR')[0].to_text()
-      inst_txt_rdata_rev = re.split('(?<=")\s+(?=")', dnssd[key]['txt'])[::-1]
-      # replace hostname.local with reverse-resolved fqdn                                                                                                                                                   
-      inst_txt_rdata_rev = [ re.sub(r'%s\.?' % dnssd[key]['hostname'], r'%s' % inst_hostname.rstrip('.'), blob) for blob in inst_txt_rdata_rev ]
-      inst_txt_rdata_rev = ' '.join(inst_txt_rdata_rev)
+        inst_type = key[1]
+        inst_fullname = dns.name.from_text(key[0] + '.' + key[1]).to_text(omit_final_dot=True)
+        #inst_hostname = dns.resolver.query(dns.reversename.from_address(dnssd[key]['address']), 'PTR')[0].to_text()
+        try:
+            inst_hostname_rev_rr = dns.resolver.query(dns.reversename.from_address(dnssd[key]['address']), 'PTR')
+        except DNSException, e:
+            continue
+        dnssd[key]['hostname_rev'] = [i.to_text(relativize=False) for i in inst_hostname_rev_rr]
+        inst_port = dnssd[key]['port']
+        inst_txt_rdata_rev = re.split(r'(?<=")\s+(?=")', dnssd[key]['txt'])[::-1]
+        for h in dnssd[key]['hostname_rev']:
+            # replace hostname.local with reverse-resolved fqdn
+            inst_txt_rdata_rev_host = [ re.sub(r'%s\.?' % dnssd[key]['hostname'], r'%s' % h.rstrip('.'), blob) for blob in inst_txt_rdata_rev ]
+            inst_txt_rdata_rev_host = ' '.join(inst_txt_rdata_rev_host)
 
-      print "%-100s %-3s %-30s" % (inst_type, 'PTR', inst_fullname)
-      print "%-100s %-3s %2d %2d %-5s %-50s" % (inst_fullname, 'SRV', 0, 0, inst_port, inst_hostname)
-      print "%-100s %-3s %s" % (inst_fullname, 'TXT', inst_txt_rdata_rev)
+            print "%-100s %-3s %-30s" % (inst_type, 'PTR', inst_fullname)
+            print "%-100s %-3s %2d %2d %-5s %-50s" % (inst_fullname, 'SRV', 0, 0, inst_port, h)
+            if (dnssd[key]['txt'] != ''):
+                print "%-100s %-3s %s" % (inst_fullname, 'TXT', inst_txt_rdata_rev_host)
 
-for stype in ['_ipp._tcp', '_printer._tcp', '_pdl-datastream._tcp', '_print-caps._tcp', '_riousbprint._tcp', '_canon-bjnp1._tcp']:
+for stype in ['_ipp._tcp', '_printer._tcp', '_pdl-datastream._tcp', '_print-caps._tcp', '_riousbprint._tcp', '_canon-bjnp1._tcp', '_http._tcp' ]:
     dnssd_search(stype=stype)
